6096527a296fcb42cd06a25b2236bb766dd67ee8
Sebastian Sdorra <sebastian.sdorra@triology.de>
2014-07-29 10:19:19 +0200

    created mapping for group

--- src/main/java/de/triology/universeadm/group/Group.java
+++ src/main/java/de/triology/universeadm/group/Group.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.triology.universeadm.group;
 
-import com.unboundid.ldap.sdk.persist.LDAPField;
+import com.google.common.collect.Lists;
+import java.util.List;
 
 /**
  *
@@ -14,6 +14,63 @@ import com.unboundid.ldap.sdk.persist.LDAPField;
  */
 public class Group
 {
-  @LDAPField(attribute = "cn", inRDN = true)
+
+  public Group()
+  {
+  }
+
+  public Group(String name)
+  {
+    this.name = name;
+  }
+
+  public Group(String name, String description, List<String> members)
+  {
+    this.name = name;
+    this.description = description;
+    this.members = members;
+  }
+
+  public Group(String name, String description, String... members)
+  {
+    this.name = name;
+    this.description = description;
+    this.members = Lists.newArrayList(members);
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public List<String> getMembers()
+  {
+    return members;
+  }
+
+  public void setMembers(List<String> members)
+  {
+    this.members = members;
+  }
+
   private String name;
+
+  private String description;
+
+  private List<String> members;
 }
