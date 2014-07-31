6967b567e420fc30b1ac4f1b2dee3accbdcd433a
Sebastian Sdorra <sebastian.sdorra@triology.de>
2014-07-31 15:43:04 +0200

    start implementation of addMember function

--- src/main/webapp/scripts/groups/controllers.js
+++ src/main/webapp/scripts/groups/controllers.js
@@ -5,10 +5,17 @@
  */
 
 
-angular.module('universeadm.groups.controllers', ['ui.bootstrap', 'universeadm.validation.directives', 'universeadm.groups.services'])
+angular.module('universeadm.groups.controllers', ['ui.bootstrap', 
+  'universeadm.validation.directives', 'universeadm.groups.services',
+  'universeadm.users.services'
+])
   .controller('groupsController', function($scope, groups){
     $scope.groups = groups;
   })
-  .controller('groupEditController', function($scope, group){
+  .controller('groupEditController', function($scope, userService, group){
     $scope.group = group;
+    
+    $scope.searchUsers = function(value){
+      return userService.search(value, 0, 5);
+    };
   });
