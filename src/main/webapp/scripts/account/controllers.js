9d69431396b41cd33322a3dd12f90eba7e8a956a
Sebastian Sdorra <sebastian.sdorra@triology.de>
2014-08-06 11:43:37 +0200

    fix membership options on account page

--- src/main/webapp/scripts/account/controllers.js
+++ src/main/webapp/scripts/account/controllers.js
@@ -5,27 +5,52 @@
  */
 
 
-angular.module('universeadm.account.controllers', ['universeadm.validation.directives', 'universeadm.account.services'])
-  .controller('accountController', function($scope, accountService, account){
-    
-    function setAccount(account){
+angular.module('universeadm.account.controllers', ['universeadm.validation.directives', 
+  'universeadm.account.services', 'universeadm.groups.services'])
+  .controller('accountController', function($scope, accountService, groupService, account) {
+
+    function setAccount(account) {
       $scope.user = account;
       $scope.master = angular.copy(account);
       $scope.confirmPassword = account.password;
     }
-    
+
     setAccount(account);
-    
-    $scope.isUnchanged = function(account){
+
+    $scope.isUnchanged = function(account) {
       return angular.equals(account, $scope.master);
     };
-    
-    $scope.save = function(account){
-      accountService.modify(account).then(function(){
+
+    $scope.save = function(account) {
+      accountService.modify(account).then(function() {
         setAccount(account);
         $scope.form.$setPristine();
-      }, function(e){
-        
+      }, function(e) {
+
       });
+    };
+    
+    $scope.searchGroups = function(value){
+      return groupService.search(value, 0, 5);
+    };
+
+    $scope.addGroup = function(group) {
+      if (group) {
+        if (account.memberOf.indexOf(group.newGroup) < 0) {
+          account.memberOf.push(group.newGroup);
+          accountService.modify(account).then(function() {
+            group.newGroup = null;
+          }, function(e) {
+
+          });
+        } else {
+          group.newGroup = null;
+        }
+      }
+    };
+
+    $scope.removeGroup = function(group) {
+      account.memberOf.splice(account.memberOf.indexOf(group), 1);
+      accountService.modify(account);
     };
   });
