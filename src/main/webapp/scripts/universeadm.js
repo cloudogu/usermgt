37102c4fc0aa87401e6a990ef251c04227f0ff2e
Michael Behlendorf <michael.behlendorf@triology.de>
2015-08-03 16:42:11 +0200

    #3 created dashboard with default widgets

--- src/main/webapp/scripts/universeadm.js
+++ src/main/webapp/scripts/universeadm.js
@@ -27,7 +27,7 @@
 
 
 angular.module('universeadm', ['angular-loading-bar', 'ngAnimate', 'restangular',
-  'ui.router', 'universeadm.navigation', 'universeadm.account.config', 
+  'ui.router', 'universeadm.navigation','universeadm.dashboard.config', 'universeadm.account.config', 
   'universeadm.users.config', 'universeadm.groups.config', 'universeadm.backup.config',
   'universeadm.settings.config'])
   .config(function(RestangularProvider, $stateProvider, $urlRouterProvider, $logProvider){
@@ -59,8 +59,8 @@ angular.module('universeadm', ['angular-loading-bar', 'ngAnimate', 'restangular'
       });
 
     // redirect start page to account
-    $urlRouterProvider.when('', '/account');
-    $urlRouterProvider.when('/', '/account');
+    $urlRouterProvider.when('', '/dashboard');
+    $urlRouterProvider.when('/', '/dashboard');
 
     // diplay error 404 for unmatched routes
     $urlRouterProvider.otherwise(function($injector){
