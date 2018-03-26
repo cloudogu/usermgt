39b91cdd251568f0b8bbe592668cc57fb78b6104
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2018-03-26 13:34:48 +0200

    Use <doguctl template> command for configuration

--- resources/resources/cas.xml.tpl
+++ resources/resources/cas.xml.tpl
@@ -1,10 +1,10 @@
-<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <cas>
-  <service>https://${FQDN}/usermgt/login/cas</service>
-  <server-url>https://${FQDN}/cas</server-url>
-  <failure-url>https://${FQDN}/usermgt/error/auth.html</failure-url>
-  <login-url>https://${FQDN}/cas/login?service=https://${FQDN}/usermgt/login/cas</login-url>
-  <logout-url>https://${FQDN}/cas/logout</logout-url>
+  <service>https://{{ .GlobalConfig.Get "fqdn" }}/usermgt/login/cas</service>
+  <server-url>https://{{ .GlobalConfig.Get "fqdn" }}/cas</server-url>
+  <failure-url>https://{{ .GlobalConfig.Get "fqdn" }}/usermgt/error/auth.html</failure-url>
+  <login-url>https://{{ .GlobalConfig.Get "fqdn" }}/cas/login?service=https://{{ .GlobalConfig.Get "fqdn" }}/usermgt/login/cas</login-url>
+  <logout-url>https://{{ .GlobalConfig.Get "fqdn" }}/cas/logout</logout-url>
   <role-attribute-names>groups</role-attribute-names>
   <administrator-role>cesManager</administrator-role>
 </cas>
--- resources/startup.sh
+++ resources/startup.sh
@@ -4,17 +4,7 @@ set -o nounset
 set -o pipefail
 
 source /etc/ces/functions.sh
-
-# create environment for templates
-FQDN=$(doguctl config --global fqdn)
-DOMAIN=$(doguctl config --global domain)
-
-LDAP_HOST=ldap
-LDAP_PORT=389
-LDAP_SERVICE=${LDAP_HOST}:${LDAP_PORT}
-LDAP_BASE_DN="o=${DOMAIN},dc=cloudogu,dc=com"
-LDAP_BIND_DN=$(doguctl config -e sa-ldap/username)
-LDAP_BIND_PASSWORD=$(/opt/apache-tomcat/webapps/usermgt/WEB-INF/cipher.sh encrypt $(doguctl config -e sa-ldap/password) | tail -1)
+export LDAP_BIND_PASSWORD=$(/opt/apache-tomcat/webapps/usermgt/WEB-INF/cipher.sh encrypt $(doguctl config -e sa-ldap/password) | tail -1)
 
 # copy resources
 if [ ! -d "/var/lib/usermgt/conf" ]; then
@@ -29,14 +19,14 @@ if [ ! -d "/var/lib/usermgt/logs" ]; then
 fi
 
 # render templates
-render_template "/var/lib/usermgt/conf/cas.xml.tpl" > "/var/lib/usermgt/conf/cas.xml"
-render_template "/var/lib/usermgt/conf/ldap.xml.tpl" > "/var/lib/usermgt/conf/ldap.xml"
+doguctl template "/var/lib/usermgt/conf/cas.xml.tpl" "/var/lib/usermgt/conf/cas.xml"
+doguctl template "/var/lib/usermgt/conf/ldap.xml.tpl" "/var/lib/usermgt/conf/ldap.xml"
 
 # create truststore, which is used in the setenv.sh
 create_truststore.sh > /dev/null
 
 # wait until ldap passed all health checks
-echo "wait unit ldap passes all health checks"
+echo "wait until ldap passes all health checks"
 if ! doguctl healthy --wait --timeout 120 ldap; then
   echo "timeout reached by waiting of ldap to get healthy"
   exit 1
