5191688c06c6bf9e75a4f38ad93086fb0a823090
Simon Klein <simon.klein@cloudogu.com>
2022-04-25 14:31:04 +0200

    Remove debug code

--- app/src/main/webapp/scripts/passwordresethandling/services.js
+++ app/src/main/webapp/scripts/passwordresethandling/services.js
@@ -6,7 +6,6 @@ angular.module('universeadm.passwordresethandling.services', ['restangular'])
             getPasswordResetDefaultValue: function (){
                 return new Promise(function (resolve) {
                     Restangular.one('account/gui_config').withHttpConfig({ cache: true}).get().then(function (result) {
-                        resolve(true);
                         // If result is undefined because of a failure in backend, make sure not to throw an error here
                         if (!!result){
                             resolve(result.pwdResetPreselected);
