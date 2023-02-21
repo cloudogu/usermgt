6d00b9bfee2562ad72a07b5404c51315190fdf1d
Christian Beyer <christian.beyer@cloudogu.com>
2023-02-21 17:25:32 +0100

    #75 add action icons and update styles

--- app/src/main/ui/src/pages/groups/Groups.tsx
+++ app/src/main/ui/src/pages/groups/Groups.tsx
@@ -1,33 +1,53 @@
 import {useEffect} from "react";
 import {Table} from "@cloudogu/ces-theme-tailwind";
 import {Group, useGroups} from "../../hooks/useGroups";
+import {TrashIcon, PencilIcon} from "@heroicons/react/24/outline";
+import {UndeletableGroups, useUndeletableGroups} from "../../hooks/useUndeletableGroups";
 
-export default function Groups(props: {title: string}) {
-  const groups: Group[]  = useGroups()
-  useEffect(() => {(document.title = props.title)}, [])
-  return <>
-    <h1 className="text-heading-font text-4xl mt-5 mb-2.5">Gruppen</h1>
-    <Table className="mt-4">
-      <Table.Head>
-        <Table.Head.Tr>
-          <Table.Head.Th>Name</Table.Head.Th>
-          <Table.Head.Th>Beschreibung</Table.Head.Th>
-          <Table.Head.Th>Benutzer</Table.Head.Th>
-          <Table.Head.Th>Aktionen</Table.Head.Th>
-        </Table.Head.Tr>
-      </Table.Head>
-      <Table.Body>
-        {groups.map(group => {
-          return <Table.Body.Tr>
-            <Table.Body.Td>
-              <span className="font-bold">{group.name}</span>
-              </Table.Body.Td>
-            <Table.Body.Td>{group.description}</Table.Body.Td>
-            <Table.Body.Td>{group.members}</Table.Body.Td>
-            <Table.Body.Td>löschen</Table.Body.Td>
-          </Table.Body.Tr>
-        })}
-      </Table.Body>
-    </Table>
-  </>;
+export default function Groups(props: { title: string }) {
+    const groups: Group[] = useGroups()
+    const undeletableGroups: UndeletableGroups = useUndeletableGroups()
+
+    useEffect(() => {
+        (document.title = props.title)
+    }, [])
+    return <>
+        <h1 className="text-heading-font text-4xl mt-5 mb-2.5 uppercase">Gruppen</h1>
+        <Table className="mt-4 text-sm">
+            <Table.Head>
+                <Table.Head.Tr className={"uppercase"}>
+                    <Table.Head.Th>Name</Table.Head.Th>
+                    <Table.Head.Th>Beschreibung</Table.Head.Th>
+                    <Table.Head.Th>Benutzer</Table.Head.Th>
+                    <Table.Head.Th className="w-0">Aktionen</Table.Head.Th>
+                </Table.Head.Tr>
+            </Table.Head>
+            <Table.Body>
+                {groups.map(group => {
+                    let isSystemGroup: boolean = undeletableGroups.includes(group.name);
+                    return <Table.Body.Tr key={group.name}>
+                        <Table.Body.Td>
+                            <span className="font-bold">{group.name}</span>
+                        </Table.Body.Td>
+                        <Table.Body.Td>
+                            <p>{group.description}</p>
+                            {isSystemGroup ?
+                                <p className="font-bold">This is a system group. It cannot be deleted.</p> : ""}
+                        </Table.Body.Td>
+                        <Table.Body.Td>{group.members?.length ?? 0}</Table.Body.Td>
+                        <Table.Body.Td className="flex justify-center">
+                            <button className={"text-text-primary hover:text-text-primary-hover"}>
+                                <PencilIcon className={"w-6 h-6"}/>
+                            </button>
+                            <button aria-label="entfernen" disabled={isSystemGroup}
+                                    className={"enabled:text-text-primary enabled:hover:text-text-primary-hover text-text-primary-disabled disabled:cursor-not-allowed"}
+                                    title="Gruppe entfernen">
+                                <TrashIcon className={"w-6 h-6"}/>
+                            </button>
+                        </Table.Body.Td>
+                    </Table.Body.Tr>
+                })}
+            </Table.Body>
+        </Table>
+    </>;
 }
--- app/src/main/ui/src/pages/users/Users.tsx
+++ app/src/main/ui/src/pages/users/Users.tsx
@@ -1,30 +1,43 @@
 import {useEffect} from "react";
 import {Table} from "@cloudogu/ces-theme-tailwind";
 import {User, useUsers} from "../../hooks/useUsers";
+import {TrashIcon, PencilIcon} from "@heroicons/react/24/outline";
 
 export default function Users(props: {title: string}) {
   const users: User[]  = useUsers()
   useEffect(() => {(document.title = props.title)}, []);
   return <>
-    <h1 className="text-heading-font text-4xl mt-5 mb-2.5">Benutzer</h1>
-    <Table className="mt-4">
+    <h1 className="text-heading-font text-4xl mt-5 mb-2.5 uppercase">Benutzer</h1>
+    <Table className="mt-4 text-sm">
       <Table.Head>
-        <Table.Head.Tr>
+        <Table.Head.Tr className={"uppercase"}>
           <Table.Head.Th>Benutzername</Table.Head.Th>
           <Table.Head.Th>Anzeigename</Table.Head.Th>
           <Table.Head.Th>E-Mail</Table.Head.Th>
-          <Table.Head.Th>Aktionen</Table.Head.Th>
+          <Table.Head.Th className="w-0">Aktionen</Table.Head.Th>
         </Table.Head.Tr>
       </Table.Head>
       <Table.Body>
         {users.map(user => {
-          return <Table.Body.Tr>
+          return <Table.Body.Tr key={user.username}>
             <Table.Body.Td>
               <span className="font-bold">{user.username}</span>
             </Table.Body.Td>
             <Table.Body.Td>{user.displayName}</Table.Body.Td>
-            <Table.Body.Td>{user.mail}</Table.Body.Td>
-            <Table.Body.Td>löschen</Table.Body.Td>
+            <Table.Body.Td>
+              <a className="hover:underline decoration-solid text-link-primary-font"
+                 href={"https://ecosystem.cloudogu.com/"}>
+                {user.mail}
+              </a>
+            </Table.Body.Td>
+            <Table.Body.Td className="flex justify-center">
+              <button className={"text-text-primary hover:text-text-primary-hover"}>
+                <PencilIcon className={"w-6 h-6"}/>
+              </button>
+              <button className={"text-text-primary hover:text-text-primary-hover"}>
+                <TrashIcon className={"w-6 h-6"}/>
+              </button>
+            </Table.Body.Td>
           </Table.Body.Tr>
         })}
       </Table.Body>
