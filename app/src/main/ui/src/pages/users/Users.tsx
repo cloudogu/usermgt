4cc57a8a677682e998b570fab904462bbf80e24b
Christian Beyer <christian.beyer@cloudogu.com>
2023-02-24 16:31:39 +0100

    #75 deactivate search field if page is loading

--- app/src/main/ui/src/pages/users/Users.tsx
+++ app/src/main/ui/src/pages/users/Users.tsx
@@ -44,7 +44,8 @@ export default function Users(props: { title: string }) {
             <div className="flex justify-between py-1">
                 <Button variant={"secondary"} className="mt-5 mb-2.5 mr-5"
                         disabled={isLoading}>{t("users.create")}</Button>
-                <Searchbar placeholder={"Filter"} clearOnSearch={false} onSearch={onSearch} className="mt-5 mb-2.5"/>
+                <Searchbar placeholder={"Filter"} clearOnSearch={false} onSearch={onSearch}
+                           className="mt-5 mb-2.5"  disabled={isLoading}/>
             </div>
         </div>
         {notification}
