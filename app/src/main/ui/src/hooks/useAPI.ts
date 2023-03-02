4b6da94cca7b29915d87c6cda7cbf18963aba21b
Christian Beyer <christian.beyer@cloudogu.com>
2023-03-02 11:55:17 +0100

    #75 fix issue with duplicated requests and abort
    - AbortController needs to be declared inside the useEffect hook
    - QueryOptions will only be updated if the values really changed

--- app/src/main/ui/src/hooks/useAPI.ts
+++ app/src/main/ui/src/hooks/useAPI.ts
@@ -16,6 +16,10 @@ export class QueryOptions {
         return this.query ?? "";
     }
 
+    get StartValue(): number {
+        return this.start ?? 0;
+    }
+
 }
 
 export type StateSetter<T> = Dispatch<SetStateAction<T | undefined>>;
@@ -24,8 +28,8 @@ export type AbortableCallbackWithOptions<T> = (signal?: AbortSignal, opts?: Quer
 export function useAPI<T>(callBack: AbortableCallbackWithOptions<T>, opts?: QueryOptions): [T | undefined , boolean, StateSetter<T>] {
     const [data, setData] = useState<T>();
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const abortController = new AbortController();
     useEffect(() => {
+        const abortController = new AbortController();
         callBack(abortController?.signal, opts)
             .then(data => {
                 setData(data);
@@ -39,7 +43,7 @@ export function useAPI<T>(callBack: AbortableCallbackWithOptions<T>, opts?: Quer
                 }
                 });
         return () => {
-            // abortController?.abort();
+            abortController.abort();
         }
     }, [opts]);
 
--- app/src/main/ui/src/hooks/useFilter.ts
+++ app/src/main/ui/src/hooks/useFilter.ts
@@ -3,7 +3,7 @@ import {QueryOptions} from "./useAPI";
 import {DEFAULT_PAGE_SIZE, calcPageStart} from "../lib/pagination";
 import {useSearchParams} from "react-router-dom";
 
-const PAGE_QUERY_PARAM = "page";
+const PAGE_QUERY_PARAM = "p";
 const SEARCH_QUERY_PARAM = "q"
 
 export function useFilter(): [(_: string) => void, (_: number) => void, () => void, QueryOptions] {
@@ -11,7 +11,7 @@ export function useFilter(): [(_: string) => void, (_: number) => void, () => vo
     const [page, query] = [+(searchParams.get(PAGE_QUERY_PARAM) ?? 1), searchParams.get(SEARCH_QUERY_PARAM) ?? ""]
     const [update, setUpdate] = useState<boolean>(false)
     const refetch = () => setUpdate(!update);
-    const [opts, setOpts] = useState<QueryOptions>(new QueryOptions(0, DEFAULT_PAGE_SIZE, query));
+    const [opts, setOpts] = useState<QueryOptions>(new QueryOptions(0, DEFAULT_PAGE_SIZE, ""));
     const updatePage = (newPage: number): void => {
         setSearchParams(current => {
             current.set(PAGE_QUERY_PARAM, `${newPage}`)
@@ -28,7 +28,13 @@ export function useFilter(): [(_: string) => void, (_: number) => void, () => vo
     };
 
     useEffect(() => {
-        setOpts(new QueryOptions(calcPageStart(page ?? 1), DEFAULT_PAGE_SIZE, query));
+        const newStart = calcPageStart(page ?? 1);
+        setOpts(prevOpts => {
+            if(prevOpts.StartValue !== newStart || prevOpts.queryString !== query) {
+                return new QueryOptions(newStart, DEFAULT_PAGE_SIZE, query);
+            }
+            return prevOpts;
+        });
     }, [searchParams, update]);
 
     return [updateQuery, updatePage, refetch, opts]
