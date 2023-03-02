67ffcbe8ee760e0e8beb4e1eec0f405f8f690474
Christian Beyer <christian.beyer@cloudogu.com>
2023-03-02 16:42:53 +0100

    #75 only show user and group lists for admin users

--- app/src/main/ui/src/main.tsx
+++ app/src/main/ui/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {createContext, useContext} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from "react-router-dom";
@@ -13,59 +13,80 @@ import usermgtIcon from './assets/usermgt_icon_detailed.svg';
 import './i18n';
 import {useTranslation} from "react-i18next";
 import {t} from "./helpers/i18nHelpers";
+import {CasUser} from "./services/CasUser";
 
 const contextPath = process.env.PUBLIC_URL || "/usermgt";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Router basename={contextPath}>
-      <Nav/>
-      <Main>
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate to={"/account"}></Navigate>}
-          />
-          <Route index path="/account"
-                 element={<Account title={t("pages.account") + " | User Management"}/>}/>
-          <Route path="/users" element={<Users title={t("pages.users") + " | User Management"}/>}/>
-          <Route path="/groups" element={<Groups title={t("pages.groups") + " | User Management"}/>}/>
-        </Routes>
-      </Main>
-    </Router>
-  </React.StrictMode>,
-)
+type ApplicationContextProps = {
+    user: CasUser;
+}
+export const ApplicationContext = createContext<ApplicationContextProps>({
+    user: {
+        principal: "default",
+        admin: false
+    }
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode>
+    <SuperMain/>
+</React.StrictMode>)
+
+function SuperMain() {
+    const [casUser] = useCasUser();
+    return <Router basename={contextPath}>
+        <ApplicationContext.Provider value={{user: casUser}}>
+            <Nav/>
+            <Main>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate to={"/account"}></Navigate>}
+                    />
+                    <Route index path="/account"
+                           element={<Account title={t("pages.account") + " | User Management"}/>}/>
+                    <Route path="/users" element={<Users title={t("pages.users") + " | User Management"}/>}/>
+                    <Route path="/groups" element={<Groups title={t("pages.groups") + " | User Management"}/>}/>
+                </Routes>
+            </Main>
+        </ApplicationContext.Provider>
+    </Router>;
+}
 
 function Nav() {
-  const location = useLocation();
-  const [user] = useCasUser();
-  const {t} = useTranslation();
-  return (
-    <>
-      <Navbar currentPath={location?.pathname ?? ""}>
-        <Navbar.LeftAlignedList>
-          <Navbar.HomeLink homeUri={"/account"}>
-            <div className={"flex items-center mr-2"}>
-              <div className={"rounded-full p-2 bg-nav-primary"}>
-                <img src={usermgtIcon} className={"w-6 h-6"} alt={t("navbar.logoAltText") as string}/>
-              </div>
-            </div>
-            <Navbar.HomeLink.LinkText>{"User Management"}</Navbar.HomeLink.LinkText>
-          </Navbar.HomeLink>
-          <Navbar.ListItem path={"/users"}>
-            <Navbar.ListItem.Icon type={"users"} className={"md:hidden"}/>
-            {t("pages.users")}
-          </Navbar.ListItem>
-          <Navbar.ListItem path={"/groups"}>
-            <Navbar.ListItem.Icon type={"groups"} className={"md:hidden"}/>
-            {t("pages.groups")}
-          </Navbar.ListItem>
-        </Navbar.LeftAlignedList>
-        <Navbar.RightAlignedList>
-          <Navbar.UserLink loggedInUser={{name: user.principal, accountUri: '/account'}}/>
-          <Navbar.LogoutLink logoutUri={`/usermgt/api/logout`}>{t("navbar.logout")}</Navbar.LogoutLink>
-        </Navbar.RightAlignedList>
-      </Navbar>
-  </>
-  )
+    const location = useLocation();
+    const {t} = useTranslation();
+    const {user} = useContext(ApplicationContext);
+    return (
+        <>
+            <Navbar currentPath={location?.pathname ?? ""}>
+                <Navbar.LeftAlignedList>
+                    <Navbar.HomeLink homeUri={"/account"}>
+                        <div className={"flex items-center mr-2"}>
+                            <div className={"rounded-full p-2 bg-nav-primary"}>
+                                <img src={usermgtIcon} className={"w-6 h-6"} alt={t("navbar.logoAltText") as string}/>
+                            </div>
+                        </div>
+                        <Navbar.HomeLink.LinkText>{"User Management"}</Navbar.HomeLink.LinkText>
+                    </Navbar.HomeLink>
+                    {user.admin ?
+                        <>
+                            <Navbar.ListItem path={"/users"}>
+                                <Navbar.ListItem.Icon type={"users"} className={"md:hidden"}/>
+                                {t("pages.users")}
+                            </Navbar.ListItem>
+                            <Navbar.ListItem path={"/groups"}>
+                                <Navbar.ListItem.Icon type={"groups"} className={"md:hidden"}/>
+                                {t("pages.groups")}
+                            </Navbar.ListItem>
+                        </> :
+                        <></>
+                    }
+                </Navbar.LeftAlignedList>
+                <Navbar.RightAlignedList>
+                    <Navbar.UserLink loggedInUser={{name: user.principal, accountUri: '/account'}}/>
+                    <Navbar.LogoutLink logoutUri={`/usermgt/api/logout`}>{t("navbar.logout")}</Navbar.LogoutLink>
+                </Navbar.RightAlignedList>
+            </Navbar>
+        </>
+    )
 }
--- app/src/main/ui/src/services/CasUser.ts
+++ app/src/main/ui/src/services/CasUser.ts
@@ -2,6 +2,7 @@ import {Axios} from "../api/axios";
 
 export type CasUser = {
     principal: string;
+    admin: boolean;
 }
 
 export const CasUserService = {
