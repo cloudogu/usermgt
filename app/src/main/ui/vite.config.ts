70848a8a5b70ce0cd4eafa237e91e9f9e125bcd6
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2023-04-19 16:34:45 +0200

    #72 refactor vite.config.ts to better support local development

--- app/src/main/ui/vite.config.ts
+++ app/src/main/ui/vite.config.ts
@@ -1,28 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import {defineConfig, loadEnv} from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.svg'],
-  define: {
-    'process.env': process.env
-  },
-  base: "/usermgt/",
-  server: {
-    proxy: {
-      "/usermgt/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/usermgt/downloads": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 2048
-  }
-})
+export default defineConfig(({command, mode}) => {
+    const config = {
+        plugins: [react()],
+        assetsInclude: ["**/*.svg"],
+        define: {
+            "process.env": process.env
+        },
+        base: "/usermgt/",
+        build: {
+            chunkSizeWarningLimit: 2048
+        }
+    };
+
+    if (command === "serve") {
+        const env = loadEnv(mode, process.cwd(), "");
+
+        config["server"] = {
+            proxy: {
+                "/usermgt/api": {
+                    target: "https://192.168.56.2",
+                    changeOrigin: true,
+                    secure: false,
+                    auth: `${env.AUTH_USER}:${env.AUTH_PASSWORD}`
+                },
+                "/usermgt/downloads": {
+                    target: "http://192.168.56.2",
+                    changeOrigin: true,
+                    secure: false,
+                    auth: `${env.AUTH_USER}:${env.AUTH_PASSWORD}`
+                }
+            },
+        };
+    }
+
+    return config;
+});
