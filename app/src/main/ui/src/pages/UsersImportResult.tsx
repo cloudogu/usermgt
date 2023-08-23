50f11c2253fbe33a6a66abef474edfe488692e9a
Simon Klein <simon.klein@cloudogu.com>
2023-08-23 15:30:12 +0200

    Render basic result table

--- app/src/main/ui/src/pages/UsersImportResult.tsx
+++ app/src/main/ui/src/pages/UsersImportResult.tsx
@@ -1,4 +1,4 @@
-import {Details, H1} from "@cloudogu/ces-theme-tailwind";
+import {Details, H1, Table} from "@cloudogu/ces-theme-tailwind";
 import React from "react";
 import {useLocation} from "react-router-dom";
 import {t} from "../helpers/i18nHelpers";
@@ -10,22 +10,48 @@ const UsersImportResult = (props: { title: string }) => {
     const {state: result} = useLocation() as Location<ImportUsersResponse>;
     useSetPageTitle(props.title);
 
-    console.log(result);
+    const createdHeadlines = Object.keys(result.created[0] ?? {}) ?? [];
+
 
     return <>
         <div className="flex flex-wrap justify-between">
             <H1 className="uppercase">{t("pages.usersImportResult")}</H1>
         </div>
-        { !result &&
+        {!result &&
             <div>
                 ERROR
             </div>
         }
-        { result &&
+        {result &&
             <div>
                 <Details>
                     <Details.Summary>Erstellt</Details.Summary>
-                    TABELLE
+                    <Table>
+                        <Table.Head>
+                            <Table.Head.Tr>
+                                {
+                                    createdHeadlines.map(k =>
+                                        <Table.Head.Th key={k}>
+                                            {k}
+                                        </Table.Head.Th>
+                                    )
+                                }
+                            </Table.Head.Tr>
+                        </Table.Head>
+                        <Table.Body>
+                            {
+                                result?.created?.map((c, i) =>
+                                    <Table.Body.Tr key={i}>
+                                        {createdHeadlines.map(h =>
+                                            <Table.Body.Td key={h}>
+                                                {(result?.created[i] as any)[h]}
+                                            </Table.Body.Td>
+                                        )}
+                                    </Table.Body.Tr>
+                                )
+                            }
+                        </Table.Body>
+                    </Table>
                 </Details>
                 <Details>
                     <Details.Summary>Aktualisiert</Details.Summary>
