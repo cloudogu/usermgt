10fb827faae054e16be91a365c13eec37f74d807
Simon Klein <simon.klein@cloudogu.com>
2023-08-25 08:19:08 +0200

    Fix protocol hook

--- app/src/main/ui/src/hooks/useProtocolList.ts
+++ app/src/main/ui/src/hooks/useProtocolList.ts
@@ -1,19 +1,8 @@
-import {ImportUsersResponse, ImportUsersService} from "../services/ImportUsers";
+import {ImportUsersService} from "../services/ImportUsers";
 import {useAPI} from "./useAPI";
 import type {QueryOptions} from "./useAPI";
-import type {PagedModel} from "@cloudogu/ces-theme-tailwind";
-import UsersImportResult from "../pages/UsersImportResult";
+import type { ProtocolsModel} from "../services/ImportUsers";
 
 export default function useProtocolList(opts: QueryOptions) {
-    const {data, isLoading} = useAPI<ProtocolsDtoModel, QueryOptions>(ImportUsersService.listImportProtocols, opts);
-    return {
-        data: {
-            protocols: (data?.protocols as ImportProtocolDto[] ?? []).map(dto => ({
-                name: dto.name,
-                date: new Date(dto.date),
-                result: dto.result,
-            } as ImportProtocol)),
-            pagination: data?.pagination,
-        } as ProtocolsModel, isLoading: isLoading
-    };
+    return useAPI<ProtocolsModel, QueryOptions>(ImportUsersService.listImportProtocols, opts);
 }
