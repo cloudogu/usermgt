fe0a81de0a0db8e79e9f2eb1380c142f05abeeb7
Nico Franzeck <nico.franzeck@cloudogu.com>
2024-02-28 13:29:48 +0100

    Delete unused exception

--- app/src/main/java/de/triology/universeadm/mail/MessageBuilderException.java
+++
@@ -1,7 +0,0 @@
-package de.triology.universeadm.mail;
-
-public class MessageBuilderException extends Exception {
-    public MessageBuilderException(String errorMsg, Throwable cause){
-        super(errorMsg, cause);
-    }
-}
