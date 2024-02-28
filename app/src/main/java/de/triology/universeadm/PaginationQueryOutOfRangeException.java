fc90631dd409af6857c9f81b4deaf96ae549c87b
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2024-02-28 13:00:12 +0100

    #112 fix integration test and code-smell

--- app/src/main/java/de/triology/universeadm/PaginationQueryOutOfRangeException.java
+++ app/src/main/java/de/triology/universeadm/PaginationQueryOutOfRangeException.java
@@ -1,12 +1,13 @@
 package de.triology.universeadm;
 
 public class PaginationQueryOutOfRangeException extends EntityException {
-    private transient final PaginationResult<?> result;
+    private final transient PaginationResult<?> result;
 
     public PaginationQueryOutOfRangeException(PaginationResult<?> result) {
         this.result = result;
     }
 
+    @java.lang.SuppressWarnings("java:S1452")
     public PaginationResult<?> getResult() {
         return result;
     }
