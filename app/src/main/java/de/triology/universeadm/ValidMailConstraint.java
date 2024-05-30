921d5c8d06bb3decd25dd2caa83927158715f8a7
Florian Deeken <florian.deeken@cloudogu.com>
2024-05-30 17:04:43 +0200

    #135 add Valid Email regex source comment

--- app/src/main/java/de/triology/universeadm/ValidMailConstraint.java
+++ app/src/main/java/de/triology/universeadm/ValidMailConstraint.java
@@ -5,6 +5,7 @@ import de.triology.universeadm.user.User;
 import java.util.regex.Pattern;
 
 public class ValidMailConstraint extends Constraint<User> {
+    // from https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#validation
     final Pattern regex = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
 
     @Override
