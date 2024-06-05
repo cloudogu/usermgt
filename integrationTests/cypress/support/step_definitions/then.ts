5c7cb7c23f2901d286ac4421e5ec25b78a2944d7
Simon Klein <simon.klein@cloudogu.com>
2024-06-05 09:36:46 +0200

    #137 Update integration-tests

--- integrationTests/cypress/support/step_definitions/then.ts
+++ integrationTests/cypress/support/step_definitions/then.ts
@@ -71,7 +71,7 @@ Then("the password-confirm entry is marked as valid", function () {
     // Since the validation is only carried out when the text field loses its focus,
     // the change of focus is effected by clicking on another position.
     cy.get('input[data-testid="password-input"]').click()
-    cy.get('input[data-testid="confirmPassword-input"]').should('have.class', 'border-textfield-success-border')
+    cy.get('input[data-testid="confirmPassword-input"]').should('not.have.class', 'border-textfield-danger-border')
     cy.get('div[data-testid="confirmPassword-input-error-errors"]').should('not.exist')
 });
 
