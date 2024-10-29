a1a266b7df82e913ab5020ce5a5ad71443861be8
Benjamin Ernst <benjamin.ernst@cloudogu.com>
2024-10-29 15:09:10 +0100

    fix eslint

--- app/src/main/ui/src/components/users/UserForm.tsx
+++ app/src/main/ui/src/components/users/UserForm.tsx
@@ -1,5 +1,4 @@
 import {deprecated_Form as Form, Details} from "@cloudogu/ces-theme-tailwind";
-import type {NotifyFunction, UseFormHandlerFunctions} from "@cloudogu/deprecated-ces-theme-tailwind";
 import {Button, H2, ListWithSearchbar} from "@cloudogu/deprecated-ces-theme-tailwind";
 import {TrashIcon} from "@heroicons/react/24/outline";
 import {twMerge} from "tailwind-merge";
@@ -7,12 +6,13 @@ import {t} from "../../helpers/i18nHelpers";
 import {useConfirmation} from "../../hooks/useConfirmation";
 import {Prompt} from "../../hooks/usePrompt";
 import useUserFormHandler from "../../hooks/useUserFormHandler";
-import type {Group} from "../../services/Groups";
 import {GroupsService} from "../../services/Groups";
 import {ConfirmationDialog} from "../ConfirmationDialog";
 import {useApplicationContext} from "../contexts/ApplicationContext";
 import HelpLink from "../helpLink";
+import type {Group} from "../../services/Groups";
 import type {User} from "../../services/Users";
+import type {NotifyFunction, UseFormHandlerFunctions} from "@cloudogu/deprecated-ces-theme-tailwind";
 
 const MAX_SEARCH_RESULTS = 10;
 
@@ -193,6 +193,5 @@ export default function UserForm<T extends User>(props: UserFormProps<T>) {
                 )
             }
         </>
-    )
-        ;
+    );
 }
