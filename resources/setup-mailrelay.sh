76a814b115ecaa03dc8e936ab2613174057307f6
Laura Gorzitze <laura.gorzitze@cloudogu.com>
2024-11-18 08:10:44 +0100

    #161 Remove 'yes' parameter for install

--- resources/setup-mailrelay.sh
+++ resources/setup-mailrelay.sh
@@ -8,7 +8,7 @@ mkdir mailrelay
 cd mailrelay
 curl -L -o emailrelay.zip https://sourceforge.net/projects/emailrelay/files/emailrelay/2.6/emailrelay-2.6-src.zip/download
 unzip emailrelay.zip
-sudo ./configure && sudo make && sudo make install -y
+sudo ./configure && sudo make && sudo make install
 echo "server plain ces-admin Ecosystem2016!" > secret.auth
 sudo emailrelay -t --as-server --forward-on-disconnect --log --verbose --log-file mailrelay.log --log-time --port 587 --forward-to localhost:1025 --server-auth ./secret.auth
 etcdctl set /config/postfix/relayhost 192.168.56.1:587
