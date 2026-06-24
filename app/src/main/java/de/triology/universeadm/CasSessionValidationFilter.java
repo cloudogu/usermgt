ed5040724527169b23c8022cae9bd1acee0d088e
Marco Bergen <marco.bergen-extern@cloudogu.com>
2026-06-24 11:26:15 +0000

    [236] Remove invalid jsession id parameter on first request

--- app/src/main/java/de/triology/universeadm/CasSessionValidationFilter.java
+++ app/src/main/java/de/triology/universeadm/CasSessionValidationFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,6 +26,23 @@ public class CasSessionValidationFilter implements Filter {
 
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession(false);
+
+        boolean hasAuth = false;
+        if (session != null) {
+            hasAuth = session.getAttribute(
+                "org.apache.shiro.subject.support.DefaultSubjectContext_PRINCIPALS_SESSION_KEY"
+            ) != null;
+        }
+
+        if (request.isRequestedSessionIdValid() && session != null && !hasAuth) {
+
+            session.invalidate();
+
+            response.sendRedirect(request.getRequestURI());
+            return;
+        }
 
         // remove ;jsessionid if session is not valid
         if (request.isRequestedSessionIdFromURL() && !request.isRequestedSessionIdValid()) {
