c908bae062439037fbb93697c478c1e1a61a5c63
Marco Bergen <marco.bergen-extern@cloudogu.com>
2026-09-07 17:26:01 +0000

    structure dogu-selection component in methods

--- app/src/main/ui/src/components/security/DoguSelection.tsx
+++ app/src/main/ui/src/components/security/DoguSelection.tsx
@@ -1,4 +1,4 @@
-import {CesIconCheck} from "@cloudogu/ces-theme-tailwind";
+import {CesIconCheck, CesIconCross} from "@cloudogu/ces-theme-tailwind";
 import React from "react";
 import "./DoguSelection.css";
 
@@ -62,44 +62,61 @@ function CheckBoxGroup({label, children, className = ""}: {
     );
 }
 
-function RadioGroupEntry({label, children, className = ""}: {
+function RadioGroupEntry({label, checked, children, className = ""}: {
     label: React.ReactNode;
-    children: React.ReactNode;
+    checked: boolean;
+    children?: React.ReactNode;
     className?: string;
 }) {
+    const hasChildren = React.Children.toArray(children).length > 0;
+
     return (
         <div className={`flex flex-col gap-0 items-start justify-start self-stretch shrink-0 relative ${className}`} >
-            <div className="bg-brand-colors-brand-weaker-brand-100 rounded-tl-rounded-lg rounded-tr-rounded-lg border-solid border-brand-colors-brand-brand-700 border-t border-r border-l pr-2 flex flex-row gap-0 items-center justify-start self-stretch shrink-0 min-h-[40px] relative overflow-hidden" >
-                {radioButton(label, true)}
+            <div className={`border-solid ${checked ? "bg-brand-colors-brand-weaker-brand-100 border-brand-colors-brand-brand-700" : "border-neutral-colors-neutral-weak-neutral-300"} ${hasChildren ? "rounded-tl-rounded-lg rounded-tr-rounded-lg border-t border-r border-l pr-2" : "rounded-rounded-lg border"} flex flex-row gap-0 items-center justify-start self-stretch shrink-0 min-h-[40px] relative overflow-hidden`} >
+                {radioButton(label, checked)}
             </div>
-            <div className="bg-neutral-colors-neutral-0 rounded-br-rounded-lg rounded-bl-rounded-lg border-solid border-brand-colors-brand-brand-700 border-r border-b border-l p-6 flex flex-col gap-4 items-end justify-start self-stretch shrink-0 relative overflow-hidden" >
-                <div className="bg-neutral-colors-neutral-0 rounded-rounded-sm flex flex-col gap-0 items-start justify-start self-stretch shrink-0 relative overflow-hidden" >
-                    {children}
-                </div>
-                <div className="flex flex-row gap-4 gap-y-0 items-center justify-end flex-wrap content-center self-stretch shrink-0 relative" >
-                    <div className="flex flex-row gap-2 items-center justify-start shrink-0 relative" >
-                        <div className="text-neutral-colors-neutral-neutral-600 text-left font-copy-paragraph-regular-font-family text-copy-paragraph-regular-font-size leading-copy-paragraph-regular-line-height font-copy-paragraph-regular-font-weight relative" >
-                            3 von 14 ausgewählt
-                        </div>
+            {hasChildren && (
+                <div className={`bg-neutral-colors-neutral-0 rounded-br-rounded-lg rounded-bl-rounded-lg border-solid ${checked ? "border-brand-colors-brand-brand-700" : "border-neutral-colors-neutral-weak-neutral-300"} border-r border-b border-l p-6 flex flex-col gap-4 items-end justify-start self-stretch shrink-0 relative overflow-hidden`} >
+                    <div className="bg-neutral-colors-neutral-0 rounded-rounded-sm flex flex-col gap-0 items-start justify-start self-stretch shrink-0 relative overflow-hidden" >
+                        {children}
                     </div>
-                    <div className="rounded-rounded-sm border-solid border-brand-colors-brand-brand-700 border-2 pt-2 pr-25 pb-2 pl-25 flex flex-row gap-05 items-center justify-center shrink-0 min-w-[40px] min-h-[40px] relative overflow-hidden" >
-                        <div className="shrink-0 w-4 h-4 relative">
-                            <img
-                                className="w-4 h-4 absolute left-0 top-0 overflow-visible"
-                                src="x0.svg"
-                            />
+                    <div className="flex flex-row gap-4 gap-y-0 items-center justify-end flex-wrap content-center self-stretch shrink-0 relative" >
+                        <div className="flex flex-row gap-2 items-center justify-start shrink-0 relative" >
+                            <div className="text-neutral-colors-neutral-neutral-600 text-left font-copy-paragraph-regular-font-family text-copy-paragraph-regular-font-size leading-copy-paragraph-regular-line-height font-copy-paragraph-regular-font-weight relative" >
+                                3 von 14 ausgewählt
+                            </div>
                         </div>
-                        <div
-                            className="pr-15 pl-15 flex flex-row gap-0 items-center justify-center shrink-0 relative"
-                        >
-                            <div
-                                className="text-brand-colors-brand-brand-700 text-left font-copy-paragraph-bold-font-family text-copy-paragraph-bold-font-size leading-copy-paragraph-bold-line-height font-copy-paragraph-bold-font-weight relative"
-                            >
-                                Auswahl aufheben
+                        <div className="rounded-rounded-sm border-solid border-brand-colors-brand-brand-700 border-2 pt-2 pr-25 pb-2 pl-25 flex flex-row gap-05 items-center justify-center shrink-0 min-w-[40px] min-h-[40px] relative overflow-hidden" >
+                            <div className="shrink-0 w-4 h-4 relative">
+                                <CesIconCross />
+                            </div>
+                            <div className="pr-15 pl-15 flex flex-row gap-0 items-center justify-center shrink-0 relative" >
+                                <div className="text-brand-colors-brand-brand-700 text-left font-copy-paragraph-bold-font-family text-copy-paragraph-bold-font-size leading-copy-paragraph-bold-line-height font-copy-paragraph-bold-font-weight relative" >
+                                    Auswahl aufheben
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            )}
+        </div>
+    );
+}
+
+function RadioGroup({label, children, className = ""}: {
+    label: React.ReactNode;
+    children?: React.ReactNode;
+    className?: string;
+}) {
+    return (
+        <div className={`flex flex-col gap-1 items-start justify-start self-stretch shrink-0 relative ${className}`} >
+            <div className="flex flex-row gap-15 items-center justify-start shrink-0 relative overflow-hidden" >
+                <div className="text-default-text text-left font-lable-label-font-family text-lable-label-font-size leading-lable-label-line-height font-lable-label-font-weight relative" >
+                    {label}
+                </div>
+            </div>
+            <div className="rounded-tl-[5px] rounded-tr-[5px] rounded-br-[5px] flex flex-col gap-2 items-start justify-start self-stretch shrink-0 relative" >
+                {children}
             </div>
         </div>
     );
@@ -107,51 +124,41 @@ function RadioGroupEntry({label, children, className = ""}: {
 
 export function DoguSelection({label}: DoguSelectionProps) {
     return (
-        <div className="flex flex-col gap-1 items-start justify-start self-stretch shrink-0 relative" >
-            <div className="flex flex-row gap-15 items-center justify-start shrink-0 relative overflow-hidden" >
-                <div className="text-default-text text-left font-lable-label-font-family text-lable-label-font-size leading-lable-label-line-height font-lable-label-font-weight relative" >
-                    {label}
-                </div>
-            </div>
-            <div className="rounded-tl-[5px] rounded-tr-[5px] rounded-br-[5px] flex flex-col gap-2 items-start justify-start self-stretch shrink-0 relative" >
-                <RadioGroupEntry label="Auswahl an Dogus">
-                    <CheckBoxGroup label="Basis">
-                        {checkBox("BlueSpice", true)}
-                        {checkBox("Cockpit", false)}
-                        {checkBox("Easy Redmine", false)}
-                        {checkBox("LOP-Assistent", true)}
-                        {checkBox("PMflexFLOW", false)}
-                        {checkBox("Redmine", false)}
-                    </CheckBoxGroup>
-                    <CheckBoxGroup label="Entwicklung">
-                        {checkBox("Grafana", true)}
-                        {checkBox("Jenkins CI", false)}
-                        {checkBox("SCM-Manager", false)}
-                        {checkBox("Smeagol", false)}
-                        {checkBox("SonarQube", false)}
-                        {checkBox("Sonatype Nexus", false)}
-                    </CheckBoxGroup>
-                    <CheckBoxGroup label="Administration" className="pr-4">
-                        {checkBox("Administration", false)}
-                        {checkBox("User Management", false)}
-                    </CheckBoxGroup>
-                </RadioGroupEntry>
-                <div
-                    className="rounded-rounded-lg border-solid border-neutral-colors-neutral-weak-neutral-300 border flex flex-row gap-2 items-start justify-start self-stretch shrink-0 min-h-[40px] relative overflow-hidden"
-                >
-                    {radioButton(
-                        <span>
-                            <span className="label-4-span">Alle Dogus</span>
-                            <span className="label-4-span2">
-                                (inklusive nachträglich installierter)
-                            </span>
-                        </span>,
-                        false
-                    )}
-                </div>
-            </div>
-        </div>
-
+        <RadioGroup label={label}>
+            <RadioGroupEntry label="Auswahl an Dogus" checked={true}>
+                <CheckBoxGroup label="Basis">
+                    {checkBox("BlueSpice", true)}
+                    {checkBox("Cockpit", false)}
+                    {checkBox("Easy Redmine", false)}
+                    {checkBox("LOP-Assistent", true)}
+                    {checkBox("PMflexFLOW", false)}
+                    {checkBox("Redmine", false)}
+                </CheckBoxGroup>
+                <CheckBoxGroup label="Entwicklung">
+                    {checkBox("Grafana", true)}
+                    {checkBox("Jenkins CI", false)}
+                    {checkBox("SCM-Manager", false)}
+                    {checkBox("Smeagol", false)}
+                    {checkBox("SonarQube", false)}
+                    {checkBox("Sonatype Nexus", false)}
+                </CheckBoxGroup>
+                <CheckBoxGroup label="Administration" className="pr-4">
+                    {checkBox("Administration", false)}
+                    {checkBox("User Management", false)}
+                </CheckBoxGroup>
+            </RadioGroupEntry>
+            <RadioGroupEntry
+                checked={false}
+                label={
+                    <span>
+                        <span className="label-4-span">Alle Dogus</span>
+                        <span className="label-4-span2">
+                            (inklusive nachträglich installierter)
+                        </span>
+                    </span>
+                }
+            />
+        </RadioGroup>
     );
 }
 
