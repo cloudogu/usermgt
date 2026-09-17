aa9b8c56b12e71de4d8aba1a6a2866a9beb31286
Marco Bergen <marco.bergen-extern@cloudogu.com>
2026-09-17 08:15:19 +0000

    add npm-internal to jenkins nmprc generation

--- integrationTests/cypress.config.ts
+++ integrationTests/cypress.config.ts
@@ -33,12 +33,12 @@ async function setupNodeEvents(
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'https://34.185.221.93',
+        baseUrl: 'https://192.168.56.2',
         env: {
             "DoguName": "usermgt",
             "MaxLoginRetries": 3,
-            "AdminUsername":  "mbergen",
-            "AdminPassword":  "u!4&E%CsSuU%uLDV",
+            "AdminUsername":  "ces-admin",
+            "AdminPassword":  "Ecosystem2016!",
             "AdminGroup":  "CesAdministrators",
             "groups" : 0,
             "users" : 0,
